125d95130df855792d82732745767ed4340c02fd
Jon <47040530+jonsterncharles@users.noreply.github.com>
2019-04-15 23:02:33 -0400

    timer and stylized audio controls

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-var audio = $("#mySoundClip")[0];
-$("nav a").mouseenter(function() {
-  audio.play();
-});
+$("div").hide();
+
+
+// Set the date we're counting down to
+var countDownDate = new Date("April 15, 2019 23:2:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="timer"
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is over.
+  if (distance < 0) {
+    clearInterval(x);
+   $("#timer").hide();
+   $("div").show();
+  }
+}, 1000);
